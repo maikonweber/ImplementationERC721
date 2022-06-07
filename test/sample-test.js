3886fee222cd31288c590e0dde4af9c54ebc2223
Maikon <m.carvalho@grouplinknetwork.com>
2022-06-07 17:51:00 -0300

    Testing with test Account Artist Is Equal a Account Constructor

--- test/sample-test.js
+++ test/sample-test.js
@@ -9,11 +9,29 @@ describe("NFT TEST ", function () {
     const nft = await nftFactory.deploy(
       "NFT_Test",
       "EST",
-      "https://dweb.link/ipns/gateway.pinata.cloud/ipfs/QmNQ961w7NG7rsBu6NnjyLcdTwhHNwdo9jTM8xt4ym2qg4",
+      "https://dweb.link/ipns/gateway.pinata.cloud/ipfs/QmNQ961w7NG7rsBu6NnjyLcdTwhHNwdo9jTM8xt4ym2qg4", //IPFS
       25,
-      "0xdD2FD4581271e230360230F9337D5c0430Bf44C0")
+      "0x8626f6940E2eb28930eFb4CeF49B2d1F2C9C1199"  // Artist Address
+      )
+
+
 
     console.log(nft.address);
     assert.isOk(nft.address);
+  });
+  it("Account Deploy Artist is Equal a Account Constructor", async function () {
+    const nftFactory = await ethers.getContractFactory("NFT");
+    const account = '0x8626f6940E2eb28930eFb4CeF49B2d1F2C9C1199' // Artist
+    const nft = await nftFactory.deploy(
+      "NFT_Test",
+      "EST",
+      "https://dweb.link/ipns/gateway.pinata.cloud/ipfs/QmNQ961w7NG7rsBu6NnjyLcdTwhHNwdo9jTM8xt4ym2qg4",
+      25,
+      account
+      )
+      const Artist = await nft.artist()
+      assert.equal(Artist, account)
+    
+  
   });
 });
