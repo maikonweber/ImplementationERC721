faf50daeaee491a724ac9656861d58a470e8f54d
maikonWeber <maikonweber@gmail.com>
2022-06-08 23:22:10 -0300

    Script Deploy

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -8,16 +8,21 @@ const hre = require("hardhat");
 async function main() {
   // Hardhat always runs the compile task when running scripts with its command
   // line interface.
-
+  const accounts = await hre.ethers.getSigners();
+  const deployer = accounts[0].address;
+  console.log(deployer)
+ 
 
   const nftFactory = await hre.ethers.getContractFactory("NFT");
   const nft = await nftFactory.deploy(
     "NFT",
     "NFT",
-    
+    "https://dweb.link/ipns/gateway.pinata.cloud/ipfs/",
+    25,
+    deployer
   )
  
-  
+  console.log(await nft.owner() , "Dono")
     
   
 
